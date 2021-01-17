4d15c5ca1307055296880a34013ac2c55a8e404c
Aayush Mishra <48150464+mishra-aayush@users.noreply.github.com>
2021-01-17 11:08:25 +0530

    Added JSON along with comments and error handling

--- ytdownloader.py
+++ ytdownloader.py
@@ -3,6 +3,45 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from webdriver_manager.firefox import GeckoDriverManager
 import youtube_dl
+import json
+
+def user_input():
+    # Channel ID: to be dealt with later
+    global channelid
+    channelid = 1
+
+    #Reading the JSON file
+    try:
+        with open('usernames.json', 'r') as openfile: 
+            json_object = json.load(openfile)
+    except:
+        print('No channels were recenetly opened')
+        json_object = {}
+
+    # Printing the 5 or less recenetly used channels
+    else:
+        print('Recently viewed channels: ')
+        counter = 0
+        for channel in json_object.keys():
+            print(channel)
+            counter += 1
+            if counter == 5:
+                break
+
+    # Taking user input
+    print('Enter the username of the channel: ')
+    channelname = input()
+    input_name = {}
+    input_name[channelname] = channelid
+    input_name.update(json_object)
+
+    # Writing into JSON file
+    with open("usernames.json", "w") as outfile: 
+        json.dump(input_name, outfile) 
+
+    # Return channel name
+    return channelname
+
 def downloader(username):
     options = Options()
     options.add_argument('-headless')
@@ -20,10 +59,9 @@ def downloader(username):
     ydl_opts = {}
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([download])
+
 def main():
-    with open('usernames.txt','r') as reader:
-        l= reader.readlines()
-    for user in l:
-        downloader(user)
+    downloader(user_input())
+
 if __name__=="__main__":
     main()
